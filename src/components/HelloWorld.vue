9d5342d1ba49f7c367fda0e871a07815cac80df7
Gema Adhi <geadwi@gmail.com>
2021-01-06 12:59:25 +0700

    first init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <v-card>
+    <v-card-title>
+      TRANSAKSI
+    </v-card-title>
+    <v-data-table
+        dense
+        :headers="headers"
+        :items="transaksi"
+        :items-per-page="10"
+        sort-by="Tanggal"
+        group-by="JenisTransaksi"
+        show-group-by
+        class="elevation-10"
+      >
+      </v-data-table>
+  </v-card>
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+
+  data: () => ({
+    headers: [
+      {
+        text: "Txn ID",
+        align: "start",
+        groupable: false,
+        sortable: true,
+        value: "TxnID"
+      },
+      { text: "Bulan/Tahun", value: "Tanggal" },
+      { text: "Jumlah", value: "Amount", sortable: false, groupable: false },
+      { text: "Code", value: "JenisTransaksi" },
+      { text: "Description", value: "Description", groupable: false },
+      { text: "Tag", value: "Tag" }
+    ],
+    transaksi: []
+  }),
+  mounted() {
+    axios
+      .get(process.env.VUE_APP_API)
+      .then(result => {
+        result.data.forEach(element => {
+          // element.Amount = element.Amount.toLocaleString("id-ID")
+          element.Amount = new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR"
+          }).format(element.Amount);
+          element.Tanggal = moment(element.Tanggal).format("MMM YY");
+        });
+        this.transaksi = result.data;
+        
+      })
+      .catch(err => {
+        alert(err);
+      });
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
